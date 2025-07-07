4777a062023da229ee8c0fadcfbdc115525523fb
darielgu <darielgutierrez332@gmail.com>
2025-07-07 14:43:02 -0700

    set up basic GoatDB template

---
+++ goat-backend/prisma/migrations/20250707214149_initial_setup/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Deal" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Deal_pkey" PRIMARY KEY ("id")
+);
--- goat-backend/server.js
+++ goat-backend/server.js
@@ -1,10 +1,24 @@
+// Packages
 import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+// Routes
+import dealRoutes from "./src/routes/dealRoutes";
+
+// Configuration
 dotenv.config();
 const PORT = process.env.PORT;
-import express from "express";
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+app.use(morgan());
+
+// Setting Routes
+app.use("/deal", dealRoutes);
+
+// Starting server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
