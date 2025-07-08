7a376b04100dd900a027617e676039ca836b93a9
darielgu <darielgutierrez332@gmail.com>
2025-07-08 15:23:21 -0700

    fixed import statement

--- goat-frontend/src/components/Pages/CRMDashboard.jsx
+++ goat-frontend/src/components/Pages/CRMDashboard.jsx
@@ -1,5 +1,4 @@
-import { Box, Typography, Paper } from "@mui/material";
-import { Box, Button } from "@mui/material";
+import { Box, Typography, Paper, Button } from "@mui/material";
 
 import React from "react";
 import SideBar from "../ReusableComponents/Sidebar";
