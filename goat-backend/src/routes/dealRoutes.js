b21970a97ef8ffe4fc61a76902acdf8194ef43ed
Oliver Majano <olivermajano01@gmail.com>
2025-07-16 09:30:48 -0700

    added retireval of all deals

--- goat-backend/src/routes/dealRoutes.js
+++ goat-backend/src/routes/dealRoutes.js
@@ -2,9 +2,10 @@ import express from "express";
 const router = express.Router();
 
 // Import deal controllers
-import { createDeal, getDealDetails } from "../controllers/deal.js";
+import { createDeal, getDealDetails, getAllDeals } from "../controllers/deal.js";
 
 // Set routes coming after /deal
 router.post("/create", createDeal);
+router.get("/all", getAllDeals);
 router.get("/:id", getDealDetails);
 export default router;
