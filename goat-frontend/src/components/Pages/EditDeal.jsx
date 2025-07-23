a938618f8ab2e2a28cb9423a9b0054ec9766ad17
costasp11 <papanicolaoucp@gmail.com>
2025-07-23 14:45:32 -0700

    deal put update routes

--- goat-frontend/src/components/Pages/EditDeal.jsx
+++ goat-frontend/src/components/Pages/EditDeal.jsx
@@ -10,6 +10,11 @@ import {
   TextField,
   IconButton,
   Tooltip,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Chip,
 } from "@mui/material";
 import {
   AttachMoney,
@@ -18,7 +23,9 @@ import {
 } from "@mui/icons-material";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import axios from "axios";
 
+const baseServer = "http://localhost:3000/deal/";
 
 // style for modal pop-up component
 const style = {
@@ -38,12 +45,13 @@ const style = {
 // selected deal passed in already
 function EditDeal({ deal }) {
 
-  // single state to hold all form data
-  const [formData, setFormData] = useState({
+  // Store the original values to reset to when canceling
+  const [originalFormData, setOriginalFormData] = useState({
     company_name: deal.deal.company_name || "",
     deal_name: deal.deal.deal_name || "",
     deal_value: deal.deal.deal_value || "",
     deal_description: deal.deal.deal_description || "",
+    stage: deal.deal.stage || "",
     service_category: deal.deal.service_category || "",
     contract_term_length: deal.deal.contract_term_length || "",
     expected_close_date: deal.deal.expected_close_date || "",
@@ -52,6 +60,10 @@ function EditDeal({ deal }) {
     slack_id: deal.participants[0].slack_id || "",
     phone_number: deal.participants[0].phone_number || "", // <-- in this format 831-512-2453
   });
+
+  // single state to hold all form data - initialize with original values
+  // this form data will be synced w/ form fields + sent to PUT request
+  const [formData, setFormData] = useState(originalFormData);
 
   // generic handler for all form fields
   const handleInputChange = (field, value) => {
@@ -63,25 +75,42 @@ function EditDeal({ deal }) {
 
   // open modal logic
   const [openModal, setOpenModal] = useState(false);
+  
   const handleOpen = () => {
+    // reset form to original values when opening modal, when put request handled 
+    // original form data will be updated when PUT request is successful
+    setFormData(originalFormData);
     setOpenModal(true);
   };
 
   const handleClose = () => {
+    // Reset form to original values when closing without updating
+    setFormData(originalFormData);
     setOpenModal(false);
   };
 
   // update deal logic
   const handleUpdateDeal = async () => {
-
-    // 1. TODO: Axios PUT Request to update the deal
-    console.log("Deal updated with data:", formData);
-    //     await axios.put(`${baseServer}update/${deal.id}`, formData);
-
-
-    // 2. Close the modal after updating
-    handleClose();
-  }
+    try {
+      console.log("Updating deal with ID:", deal.deal.id);
+      console.log("Form data to update:", formData);
+      // 1. Axios PUT request to update deal details (selected deal's id)
+      const response = await axios.put(
+        `${baseServer}updateDetails/${deal.deal.id}`, 
+        formData
+      );
+      
+      // 2. Update was successful, close modal and change original form data
+      console.log("Update response:", response.data);
+      setOpenModal(false);
+      setOriginalFormData(formData); // Update original form data with new values
+      
+    } catch (error) {
+      console.error("Error updating deal:", error);
+      // Handle error - maybe show a notification
+      // Don't close modal on error so user can try again
+    }
+  };
 
   return (
     <>
@@ -110,7 +139,7 @@ function EditDeal({ deal }) {
           />
         </IconButton>
       </Tooltip>
-
+      
       {/* Modal component to edit deal */}
       <Modal
         open={openModal}
@@ -222,14 +251,29 @@ function EditDeal({ deal }) {
                         Stage
                       </Typography>
                       {/* TODO: no deal stage */}
-                      <TextField
-                        value={""}
-                        // onChange={(e) =>
-                        //   handleInputChange("deal_stage", e.target.value)
-                        // }
-                        variant="outlined"
-                        size="small"
-                      />
+                      <FormControl sx={{ minWidth: 223 }} size="small">
+                        <InputLabel id="demo-simple-select-label">
+                          Stage
+                        </InputLabel>
+                        <Select
+                          labelId="demo-simple-select-label"
+                          id="demo-simple-select"
+                          value={formData.stage || ""}
+                          onChange={(e) =>
+                            handleInputChange("stage", e.target.value) // value in menu item?
+                          }
+                        >
+                          <MenuItem value={"prospecting"}>Prospecting</MenuItem>
+                          <MenuItem value={"qualification"}>Qualification</MenuItem>
+                          <MenuItem value={"proposal"}>Proposal</MenuItem>
+                          <MenuItem value={"negotiation"}>
+                            Negotiation
+                          </MenuItem>
+                          <MenuItem value={"closed_won"}>Closed Won</MenuItem>
+                          <MenuItem value={"closed_lost"}>Closed Lost</MenuItem>
+                          <MenuItem value={"active"}>Active</MenuItem>
+                        </Select>
+                      </FormControl>
                     </Box>
                   </Box>
                   {/* Last text-field container */}
@@ -393,7 +437,7 @@ function EditDeal({ deal }) {
                       Product/Service
                     </Typography>
                     <TextField
-                    // TODO: no product/service
+                      // TODO: no product/service
                       value={""}
                       // onChange={(e) =>
                       //   handleInputChange("product", e.target.value)
@@ -413,7 +457,10 @@ function EditDeal({ deal }) {
                     <TextField
                       value={formData.contract_term_length}
                       onChange={(e) =>
-                        handleInputChange("contract_term_length", e.target.value)
+                        handleInputChange(
+                          "contract_term_length",
+                          e.target.value
+                        )
                       }
                       variant="outlined"
                       size="small"
@@ -428,7 +475,7 @@ function EditDeal({ deal }) {
                       Tag(s)
                     </Typography>
                     <TextField
-                    // TODO: no tags
+                      // TODO: no tags
                       value={""}
                       // onChange={(e) =>
                       //   handleInputChange("tags", e.target.value)
@@ -459,11 +506,22 @@ function EditDeal({ deal }) {
             </Box>
           </Box>
           <Box>
-            <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}>
-              {/* TODO: actually update deal once complete */}
-              <Button size="small" variant="contained" onClick={handleUpdateDeal}>
-                {" "}
-                Update Deal{" "}
+            <Box sx={{ display: "flex", justifyContent: "flex-end", gap: 2, mt: 2 }}>
+              {/* Cancel Button */}
+              <Button
+                size="small"
+                variant="outlined"
+                onClick={handleClose}
+              >
+                Cancel
+              </Button>
+              {/* Update Deal Button */}
+              <Button
+                size="small"
+                variant="contained"
+                onClick={handleUpdateDeal}
+              >
+                Update Deal
               </Button>
             </Box>
           </Box>
